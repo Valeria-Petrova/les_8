27d40ac32fc786c856b30365f90091f1d4f5a855
Valeria <valeria89_07@mail.ru>
2022-12-20 14:56:51 +0300

    задача 62

---
+++ 62/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+//Например, на выходе получается вот такой массив:
+//01 02 03 04
+//12 13 14 05
+//11 16 15 06
+//10 09 08 07
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк массива: ");
+int n = int.Parse(ReadLine());
+int[,] array = GetArray(n);
+PrintArray(array);
+
+
+int[,] GetArray(int n)
+{
+    int[,] result = new int[n, n];
+    int start = 1;
+    int i = 0;
+    int j = 0;
+    while (start <= result.GetLength(0) * result.GetLength(1))
+    {
+        result[i, j] = start;
+        start++;
+        if (i <= j + 1 && i + j < result.GetLength(1) - 1)
+        j++;
+        else if (i < j && i + j >= result.GetLength(0) - 1)
+        i++;
+        else if (i >= j && i + j > result.GetLength(1) - 1)
+        j--;
+        else
+        i--;
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] / 10 <= 0)
+            {
+                Write($"0{array[i, j]} ");
+            }
+            else
+            {
+                Write($"{array[i, j]} ");
+            }
+        }
+        WriteLine();
+    }
+}
